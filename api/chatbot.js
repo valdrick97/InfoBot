21244c73b8d835960a40929b9bad5c82c4247c56
Worker <sidney.rozas@gmail.com>
2025-01-31 18:48:06 -0600

    Create chatbot.js

---
+++ api/chatbot.js
@@ -0,0 +1,13 @@
+const express = require('chatbot');
+const app = express();
+
+app.use(express.json());
+
+app.post('/chat', (req, res) => {
+  const userInput = req.body.message;
+  // Process user input and generate a response (your chatbot logic)
+  const response = "This is the bot's response to: " + userInput;
+  res.json({ response });
+});
+
+module.exports = app;
